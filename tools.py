9433584456b793da6f167bdae2e57d01d32517cc
Enzo JUPILLE <enzojupille@gmail.com>
2024-01-04 20:27:25 +0100

    Rework DB management

--- tools.py
+++ tools.py
@@ -21,7 +21,7 @@ class Mod:
             self.primary = primary
             self.secondaries = secondaries
         else:
-            print("The mod you are trying to build has too much or not enough secondary stats (need 4, " + len(secondaries) + " provided)")
+            print("The mod you are trying to build has too much or not enough secondary stats (need 4, " + str(len(secondaries)) + " provided)")
 
     @classmethod
     def getMod(self):
@@ -35,7 +35,7 @@ class Mod:
             self.primary = primary
             self.secondaries = secondaries
         else:
-            print("The mod you are trying to build has too much or not enough secondary stats (need 4, " + len(secondaries) + " provided)")
+            print("The mod you are trying to build has too much or not enough secondary stats (need 4, " + str(len(secondaries)) + " provided)")
     
     # @classmethod
     # def __str__(self):
@@ -107,9 +107,19 @@ def not_a_valid_number(value, threshold):
         return False
     return (value <= 0 and value >= threshold)
 
-def mostNeededModSet():
-    # TODO
-    return
+"""
+Remove duplicate from list
+IN: List
+OUT: List
+EX:
+removeDuplicateFromList([[1, 2], [4], [5, 6, 2], [1, 2], [3], [4]]) = [[1, 2], [3], [4], [5, 6, 2]]
+"""
+def removeDuplicateFromList(list):
+    noDuplicate = []
+    for i in list:
+        if i not in noDuplicate:
+            noDuplicate.append(i)
+    return noDuplicate
 
 """
 Search strings from list to another list
@@ -122,3 +132,42 @@ checkFromListToList([abc, def, ghi], [abd-156, ajc-198, dea-186, dgd-123]) = FAL
 """
 def checkFromListToList(toLookFor, toLookIn):
     return any(ext in toLookFor for ext in toLookIn)
+
+"""
+Return sets value (/!\ At this point we already verified all set from sets are in potentialSet)
+IN : List
+OUT : INT
+EX:
+computeSetValue(["criticaldamage","criticalchance"]) = 6 (GOOD)
+computeSetValue(["defense","criticalchance","potency"]) = 6 (GOOD)
+computeSetValue(["speed","criticaldamage"]) = 8 (BAD)
+"""
+def computeSetValue(sets):
+    res = 0
+    for set in sets:
+        if set in constants.largeSets:
+            res += 4
+        else:
+            res += 2
+    return res
+
+"""
+Return all possible mods from given informations
+IN: List, List, List
+Out: List[List]
+EX: 		
+createModsFromLine(["speed","health"], ["offense%","speed","defense%","protection%","protection%","potency%"], ["potency%","speed","protection%","health%","offense%"]) 
+= [["speed","square","offense%",["potency%","speed","protection%","health%"]], ["speed","arrow","speed",["potency%","protection%","health%,offense%"]], ...]
+"""
+def createModsFromLine(sets, primaries, secondaries, done):
+    res = []
+    sets = removeDuplicateFromList(sets)
+    for set in sets:
+        for idx,primary in enumerate(primaries):
+            secondariesToKeep = []
+            for secondary in secondaries:
+                if secondary != primary and len(secondariesToKeep) < 4:
+                    secondariesToKeep.append(secondary)
+            secondariesToKeep = sort_secondaries(secondariesToKeep)
+            res.append([set, constants.potentialShape[idx],primary, secondariesToKeep, done[idx]])
+    return removeDuplicateFromList(res)
--- tsvManagement.py
+++ tsvManagement.py
@@ -15,28 +15,53 @@ def checkTsv():
     while not eof:
         with open(constants.tsvFileName,'r') as swgohRead:
             reader = csv.DictReader(swgohRead, delimiter="\t")
-            lineNumber = 0
+            lineNumber = 1
             for row in reader:
                 lineNumber += 1
                 if row.values() is not None:
+                    setErrors = 0
+                    sets = row['sets'].split(",")
                     primaries = row['primaries'].split(",")
                     secondaries = row['secondaries'].split(",")
-                    for i in range(len(primaries)):
-                        # primary isn't speed and doesn't with % -> problem (speed is the only flat primary)
-                        # we ignore blank values (character mods aren't filled yet)
-                        if primaries[i] != "speed" and primaries[i] != "" and primaries[i] != "-" and primaries[i][-1] != "%":
+                    if len(sets) > 1 and len(primaries) > 1 and len(secondaries) > 1:
+                        for i in range(len(sets)):
+                            if sets[i] not in constants.potentialSet:
+                                nbErrors += 1
+                                setErrors += 1
+                                print("Set error, line " + str(lineNumber) + " on " + sets[i] + ", set number " + str(i+1) + " must be one of those values : " + str(constants.potentialSet))
+                        if setErrors == 0 and tools.computeSetValue(sets) != 6:
                             nbErrors += 1
-                            print("Primary error, line: " + str(lineNumber))
-                    for i in range(len(secondaries)):
-                        # only %only secondaries 
-                        if secondaries[i] == "tenacity" or secondaries[i] == "potency" or secondaries[i] == "criticalchance":
+                            print("Set error, line " + str(lineNumber) + " on " + sets[i] + ", you either put set too much sets or not enough " + str(sets))
+                        for i in range(len(primaries)):
+                            if primaries[i] not in constants.potentialPrimary[i]:
+                                nbErrors += 1
+                                print("Primary error, line " + str(lineNumber) + " on " + primaries[i] + ", primary number " + str(i+1) + " must be one of those values : " + str(constants.potentialPrimary[i]))
+                            # primary isn't speed and doesn't with % -> problem (speed is the only flat primary)
+                            # we ignore blank values (character mods aren't filled yet)
+                            elif primaries[i] != "speed" and primaries[i] != "" and primaries[i] != "-" and not primaries[i].endswith("%"):
+                                nbErrors += 1
+                                print("Primary error, line " + str(lineNumber) + " on " + primaries[i] + " (% missing)")
+                            elif primaries[i].endswith("%%"):
+                                nbErrors += 1
+                                print("Primary error, line " + str(lineNumber) + " on " + primaries[i] + " (double %%)")
+                        for i in range(len(secondaries)):
+                            if secondaries[i] not in constants.potentialSecondary:
+                                nbErrors += 1
+                                print("Secondaries error, line " + str(lineNumber) + " on " + secondaries[i] + ", secondary number " + str(i+1) + " must be one of those values : " + str(constants.potentialSecondary))
+                            # only %only secondaries 
+                            elif secondaries[i] == "tenacity" or secondaries[i] == "potency" or secondaries[i] == "criticalchance":
+                                nbErrors += 1
+                                print("Secondaries error, line " + str(lineNumber) + " on " + secondaries[i] + " (% missing)")
+                            elif secondaries[i].endswith("%%"):
+                                nbErrors += 1
+                                print("Secondaries error, line " + str(lineNumber) + " on " + secondaries[i] + " (double %%)")
+                        # we ignore "-" for duplicates
+                        secondariesDuplicate = tools.list_duplicates(secondaries, "-")
+                        if len(secondariesDuplicate) > 0:
                             nbErrors += 1
-                            print("Secondaries error, line: " + str(lineNumber) + " on secondary number " + str(i))
-                    # we ignore "-" for duplicates
-                    secondariesDuplicate = tools.list_duplicates(secondaries, "-")
-                    if len(secondariesDuplicate) > 0:
-                        nbErrors += 1
-                        print("Duplicate error, line: " + str(lineNumber) + " on " + str(secondariesDuplicate))
+                            print("Duplicate error, line " + str(lineNumber) + " on " + str(secondariesDuplicate[0][0]) + ", secondary number " + str(secondariesDuplicate[0][1][0]) + " and " + str(secondariesDuplicate[0][1][1]) + " are duplicate.")
+                    else:
+                        print("Line " + str(lineNumber) + " (" + row['name'] + ") has been ignored (incomplete)")
                 else:
                     nbErrors = -1
                     print("Something's wrong with your file format, tabulation might be missing somewhere ..")
@@ -61,36 +86,48 @@ def refractorTsv(nbTSVErrors):
         reader = csv.DictReader(swgohRead, delimiter="\t")
         writer = csv.DictWriter(swgohWrite, reader.fieldnames, delimiter="\t")
         writer.writeheader()
+        startingError = nbTSVErrors
         while nbTSVErrors > 0 and not eof:
             for row in reader:
                 if row.values() is not None:
+                    sets = row['sets'].split(",")
                     primaries = row['primaries'].split(",")
                     secondaries = row['secondaries'].split(",")
-                    for i in range(len(primaries)):
-                        if primaries[i] != "speed" and primaries[i] != "" and primaries[i] != "-" and primaries[i][-1] != "%":
-                            primaries[i] += "%"
-                            nbTSVErrors -= 1
-                    for i in range(len(secondaries)):
-                        if secondaries[i] == "tenacity" or secondaries[i] == "potency" or secondaries[i] == "criticalchance":
-                            secondaries[i] += "%"
-                            nbTSVErrors -= 1
-                    sortedSecondariesDuplicate = sorted(tools.list_duplicates(secondaries))
-                    for dup in sortedSecondariesDuplicate:
-                        combined = "\t".join(potentialSecondariesDuplicate)
-                        if dup[0] in combined:
-                            if "%" in dup[0]:
-                                secondaries[dup[1][1]] = secondaries[dup[1][1]][:-1]
-                            else:
-                                secondaries[dup[1][0]] += "%"
+                    if len(sets) > 1 and len(primaries) > 1 and len(secondaries) > 1:
+                        for i in range(len(primaries)):
+                            if primaries[i] != "speed" and primaries[i] != "" and primaries[i] != "-" and primaries[i][-1] != "%":
+                                primaries[i] += "%"
                                 nbTSVErrors -= 1
-                    row['primaries'] = ",".join(primaries)
-                    row['secondaries'] = ",".join(secondaries)
-                    writer.writerow(row)
+                            if primaries[i].endswith("%%"):
+                                primaries[i] = primaries[i][:-1]
+                                nbTSVErrors -= 1
+                        for i in range(len(secondaries)):
+                            if secondaries[i] == "tenacity" or secondaries[i] == "potency" or secondaries[i] == "criticalchance":
+                                secondaries[i] += "%"
+                                nbTSVErrors -= 1
+                            if secondaries[i].endswith("%%"):
+                                secondaries[i] = secondaries[i][:-1]
+                                nbTSVErrors -= 1
+                        sortedSecondariesDuplicate = sorted(tools.list_duplicates(secondaries))
+                        for dup in sortedSecondariesDuplicate:
+                            combined = "\t".join(potentialSecondariesDuplicate)
+                            if dup[0] in combined:
+                                if "%" in dup[0]:
+                                    secondaries[dup[1][1]] = secondaries[dup[1][1]][:-1]
+                                else:
+                                    secondaries[dup[1][0]] += "%"
+                                nbTSVErrors -= 1
+                        row['primaries'] = ",".join(primaries)
+                        row['secondaries'] = ",".join(secondaries)
+                        writer.writerow(row)
                 else:
                     print("Something's wrong with your file format, tabulation might be missing somewhere ..")
                     nbTSVErrors = -1
             eof = True
     os.remove(dummyName)
+    print(str(startingError - nbTSVErrors) + " errors has been fixed.")
     if nbTSVErrors > 0:
         nbTSVErrors = -1
+    if nbTSVErrors == 0:
+        print("TSV file has been fixed succesfully.")
     return nbTSVErrors 
